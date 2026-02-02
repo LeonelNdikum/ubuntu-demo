e384019a02e2876fec12ba54447dc61834c0561b
ndikumleonelkubong@yahoo.com <ndikumleonelkubong@yahoo.com>
2026-02-02 00:56:29 +0000

    adding endering

---
+++ endering.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Welcome Leonel, please select your starting class"
+
+echo "1- samurai"
+echo "2- prisoner"
+echo "3- Prophet"
+
+echo "Please enter class number from 1-3"
+
+read class
+
+if [[ $class==1 ]]; then
+
+                type="samurai"
+                hp=10
+                attack=20
+
+elif [[ $class==2 ]]; then
